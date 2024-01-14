7b025136e6332387baa43fd949c98f81e67d1ec6
Suryansh Prajapati <itssuryanshprajapati@gmail.com>
2024-01-14 11:32:11 +0530

    make the API_KEY in local properties

--- app/src/main/java/com/weather/weatherapp/Constants.kt
+++ app/src/main/java/com/weather/weatherapp/Constants.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import com.example.weatherapp.BuildConfig
 
 object Constants {
 
-    const val APP_ID: String = "ADD YOUR API HERE"
+    // Put your API_KEY in local.properties. In this format -
+//    OPENWEATHER_API_KEY= YOUR API KEY
+
+
+    const val APP_ID: String = BuildConfig.OPENWEATHER_API_KEY
     const val BASE_URL: String = "http://api.openweathermap.org/data/"
     const val METRIC_UNIT: String = "metric"
 
@@ -16,9 +21,10 @@ object Constants {
      */
     fun isNetworkAvailable(context: Context): Boolean {
         // It answers the queries about the state of network connectivity.
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val network      = connectivityManager.activeNetwork ?: return false
+            val network = connectivityManager.activeNetwork ?: return false
             val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
             return when {
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
