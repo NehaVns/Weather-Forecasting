b4ca1e7e9987bb36af4a5eab808c2726bac5cf9e
Suryansh Prajapati <itssuryanshprajapati@gmail.com>
2024-02-06 12:10:26 +0530

    added the splash screen

---
+++ app/src/main/java/com/weather/weatherapp/Splash_Screen.kt
@@ -0,0 +1,52 @@
+package com.weather.weatherapp
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.ActivitySplashScreenBinding
+
+class Splash_Screen : AppCompatActivity() {
+    private var binding: ActivitySplashScreenBinding? = null
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = getColor(R.color.white)
+        }
+
+        setContentView(binding?.root)
+
+
+        val typeface: Typeface =
+            Typeface.createFromAsset(assets, "museo.ttf")
+        binding?.tvTaaveezHeading?.typeface = typeface
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(1000)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    val intent = Intent(
+                        this@Splash_Screen,
+                        MainActivity::class.java
+                    )
+                    startActivity(intent)
+
+                    finish()
+                }
+            }
+        };thread.start()
+    }
+}
